6c4a2db6aa49e8572bc718d12356bf55a9e12976
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-03-31 11:24:22 +0700

    modifikasi soal 2

--- MahasiswaDemo06.java
+++ MahasiswaDemo06.java
@@ -1,25 +1,39 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo06 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi06 list = new MahasiswaBerprestasi06();
 
-        Mahasiswa06 m1 = new Mahasiswa06("123", "Zidan", "2A", 3.2);
-        Mahasiswa06 m2 = new Mahasiswa06("124", "Ayu", "2A", 3.5);
-        Mahasiswa06 m3 = new Mahasiswa06("125", "Sofi", "2A", 3.1);
-        Mahasiswa06 m4 = new Mahasiswa06("126", "Sita", "2A", 3.9);
-        Mahasiswa06 m5 = new Mahasiswa06("127", "Miki", "2A", 3.7);
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Data mahasiswa ke-" + (i + 1));
 
-        System.out.println("Data mahasiswa sebelum sorting:");
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa06 m = new Mahasiswa06(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+
+        System.out.println("\nData mahasiswa sebelum sorting:");
         list.tampil();
 
-        System.out.println("Data mahasiswa setelah sorting berdadasarkan IPK (DESC):");
+        System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC):");
         list.bubbleSort();
         list.tampil();
     }
-    
 }
