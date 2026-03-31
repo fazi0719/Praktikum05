eac1931160edb7c078c03e3b42cbf8645f3f6df2
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-03-31 10:57:32 +0700

    praktikum 2

---
+++ MahasiswaBerprestasi06.java
@@ -0,0 +1,30 @@
+public class MahasiswaBerprestasi06 {
+    Mahasiswa06 [] listMhs = new Mahasiswa06[5];
+    int idx;
+    // method
+    void tambah (Mahasiswa06 m){
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh.");
+        }
+    }
+     void tampil() {
+        for (Mahasiswa06 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("-------------------");
+        }
+    }
+    void bubbleSort() {
+        for (int i=0; i<listMhs.length-1; i ++){
+            for (int j=1; j<listMhs.length-i; j++){
+                if (listMhs[j].ipk > listMhs[j-1].ipk){
+                    Mahasiswa06 temp = listMhs[j];
+                    listMhs[j] = listMhs[j-1];
+                    listMhs[j-1] = temp;
+                }
+            }
+        }
+}
+}
