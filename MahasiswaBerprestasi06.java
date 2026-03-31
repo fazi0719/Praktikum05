3a7fe0e4d3ae2977ba15e7760cbe251266d84272
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-03-31 12:27:16 +0700

    ..

--- MahasiswaBerprestasi06.java
+++ MahasiswaBerprestasi06.java
@@ -40,4 +40,19 @@ void SelectionSort() {
         listMhs[i] = tmp;
     }
 }
+// proses sorting dengan cara descending
+
+void InsertionSort() {
+    for (int i = 1; i < listMhs.length; i++) {
+        Mahasiswa06 temp = listMhs[i];
+        int j = i;
+
+        while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+            listMhs[j] = listMhs[j - 1];
+            j--;
+        }
+
+        listMhs[j] = temp;
+    }
+}
 }
